049abf9e041cccdad6446f8dd7dd0e322d7c2976
James Herdman <james.herdman@gmail.com>
2019-03-22 18:00:00 +0000

    Use imgix-photo Component
    No need to implement this shit twice.

--- addon/components/avatar-image.js
+++ addon/components/avatar-image.js
@@ -1,6 +1,5 @@
-import { computed } from '@ember/object';
 import Component from '@ember/component';
-import { htmlSafe } from '@ember/string';
+
 import layout from '../templates/components/avatar-image';
 
 /**
@@ -10,31 +9,11 @@ import layout from '../templates/components/avatar-image';
  * https://docs.imgix.com/
  */
 export default Component.extend({
+  // alt: optional
+  // params: optional
   // url: optional
-  // alt: optional -- highly recommended
-  // w: optional
-  // h: optional
-  // fit: optional
-  // facepad: optional
   // avatarImageClass: optional
   // avatarIconClass: optional
 
   layout,
-
-  urlParams: computed('fit', 'facepad', 'faceindex', 'w', 'h', function() {
-    let params = this.getProperties('fit', 'facepad', 'faceindex', 'w', 'h');
-    let opts = `dpr=${window.devicePixelRatio}`;
-
-    for (let param in params) {
-      let value = params[param];
-
-      if (typeof value === 'undefined') {
-        continue;
-      }
-
-      opts += `&${param}=${value}`;
-    }
-
-    return htmlSafe(opts);
-  }),
 });
--- tests/integration/components/avatar-image-test.js
+++ tests/integration/components/avatar-image-test.js
@@ -1,145 +1,87 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { find, render } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | avatar image', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('default params are appended', async function(assert) {
-    let expectedParams = `?dpr=${window.devicePixelRatio}`;
-    let url = '/foo.jpg';
+  test('when URL present', async function(assert) {
+    const alt = 'alt';
+    const url = '/foo.jpg';
 
-    this.set('url', url);
+    this.setProperties({
+      alt,
+      url,
+      params: {
+        w: 20,
+        h: 30,
+      },
+    });
 
-    await render(hbs`<AvatarImage @url={{url}} />`);
+    const selector = '[data-test-selector="avatar-image"]';
 
-    let avatarImg = find('[data-test-selector="avatar-image"]');
+    await render(hbs`<AvatarImage
+      @alt={{alt}}
+      @url={{url}}
+      @params={{params}}
+      @avatarImageClass="piggy"
+    />`);
 
-    assert.equal(
-      avatarImg.getAttribute('src').indexOf(url),
-      0,
-      'does not add external host information'
-    );
+    assert
+      .dom(selector)
+      .hasAttribute(
+        'src',
+        new RegExp(`^${url}`),
+        'does not prepend host information'
+      );
 
-    assert.notEqual(
-      avatarImg.getAttribute('src').indexOf(expectedParams),
-      -1
-    );
+    assert
+      .dom(selector)
+      .hasAttribute(
+        'src',
+        new RegExp(/&w=20/),
+        'passes attributes to underlying component'
+      );
+
+    assert
+      .dom(selector)
+      .hasAttribute(
+        'alt',
+        alt
+      );
+
+    assert
+      .dom(selector)
+      .hasAttribute(
+        'width',
+        '20',
+        'sets width'
+      );
+
+    assert
+      .dom(selector)
+      .hasAttribute(
+        'height',
+        '30',
+        'sets height'
+      );
+
+    assert
+      .dom(selector)
+      .hasClass(
+        'piggy',
+        'sets class names on image'
+      );
   });
 
-  test('supports width param', async function(assert) {
-    let url = '/foo.jpg';
-
-    this.set('url', url);
-
-    await render(hbs`<AvatarImage @url={{url}} @w={{20}} />`);
-
-    let img = find('[data-test-selector="avatar-image"]');
-    let src = img.getAttribute('src');
-
-    assert.equal(src.indexOf(url), 0, 'does not add external host information');
-
-    assert.notEqual(src.indexOf('&w=20'), -1, 'sets width parameter for Imgix');
-    assert.equal(img.getAttribute('width'), 20, 'sets width attribute on image tag');
-  });
-
-  test('supports height param', async function(assert) {
-    let url = '/foo.jpg';
-
-    this.set('url', url);
-
-    await render(hbs`<AvatarImage @url={{url}} @h={{20}} />`);
-
-    let img = find('[data-test-selector="avatar-image"]');
-    let src = img.getAttribute('src');
-
-    assert.equal(src.indexOf(url), 0, 'does not add external host information');
-
-    assert.notEqual(src.indexOf('&h=20'), -1, 'sets height parameter for Imgix');
-    assert.equal(img.getAttribute('height'), 20, 'sets height attribute on image tag');
-  });
-
-  test('supports fit param', async function(assert) {
-    let url = '/foo.jpg';
-
-    this.set('url', url);
-    this.set('fit', 'facearea');
-
-    await render(hbs`<AvatarImage @url={{url}} @fit={{fit}} />`);
-
-    let img = find('[data-test-selector="avatar-image"]');
-    let src = img.getAttribute('src');
-
-    assert.equal(src.indexOf(url), 0, 'does not add external host information');
-
-    assert.notEqual(src.indexOf('&fit=facearea'), -1);
-  });
-
-  test('supports facepad param', async function(assert) {
-    let url = '/foo.jpg';
-
-    this.set('url', url);
-
-    await render(hbs`<AvatarImage @url={{url}} @facepad={{3}} />`);
-
-    let img = find('[data-test-selector="avatar-image"]');
-    let src = img.getAttribute('src');
-
-    assert.equal(src.indexOf(url), 0, 'does not add external host information');
-
-    assert.notEqual(src.indexOf('&facepad=3'), -1);
-  });
-
-  test('supports faceindex param', async function(assert) {
-    let url = '/foo.jpg';
-
-    this.set('url', url);
-
-    await render(hbs`<AvatarImage @url={{url}} @faceindex={{1}} />`);
-
-    let img = find('[data-test-selector="avatar-image"]');
-    let src = img.getAttribute('src');
-
-    assert.equal(src.indexOf(url), 0, 'does not add external host information');
-
-    assert.notEqual(src.indexOf('&faceindex=1'), -1);
-  });
-
-  test('does not insert undefined attributes for Imgix', async function(assert) {
-    let url = '/foo.jpg';
-
-    this.set('url', url);
-
-    await render(hbs`<AvatarImage @url={{url}} />`);
-
-    let img = find('[data-test-selector="avatar-image"]');
-    let src = img.getAttribute('src');
-
-    assert.equal(src.indexOf(url), 0, 'does not add external host information');
-
-    assert.equal(src.indexOf('&faceindex=undefined'), -1);
-  });
-
-  test('other avatar image attributes', async function(assert) {
-    let url = '/foo.jpg';
-
-    this.set('url', url);
-
-    await render(hbs`<AvatarImage @url={{url}} @alt="Cheesus" @avatarImageClass="piggy" />`);
-
-    let img = find('[data-test-selector="avatar-image"]');
-
-    assert.equal(img.getAttribute('alt'), 'Cheesus', 'sets "alt" attribute');
-    assert.dom(img).hasClass('piggy', 'sets class names on image');
-  });
-
-  test('no url provided', async function(assert) {
+  test('when URL blank', async function(assert) {
     await render(hbs`<AvatarImage @avatarIconClass="foo" />`);
 
-    let avatarIcon = find('[data-test-selector="avatar-icon"]');
+    const selector = '[data-test-selector="avatar-icon"]';
 
-    assert.ok(avatarIcon, 'displays blank avatar icon');
-    assert.dom(avatarIcon).hasClass('foo', 'sets class names on avatar icon');
+    assert.dom(selector).exists();
+
+    assert.dom(selector).hasClass('foo', 'sets class names on avatar icon');
   });
 });
