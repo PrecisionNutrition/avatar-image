5a27ed65a10994c5cb3f78bbc7d57ab016d33c2d
Benjamin Borowski <ben.borowski@precisionnutrition.com>
2019-09-13 14:58:03 -0700

    [SM-29] adds https to gravatar urls

--- addon/helpers/gravatar-image-url.js
+++ addon/helpers/gravatar-image-url.js
@@ -4,7 +4,7 @@ import md5 from 'ember-md5';
 export function gravatarImageUrl([email], { size='' }) {
   let hashedEmail = md5(email);
 
-  return `//www.gravatar.com/avatar/${hashedEmail}.jpg?d=mm&s=${size}`;
+  return `https://www.gravatar.com/avatar/${hashedEmail}.jpg?d=mm&s=${size}`;
 }
 
 export default helper(gravatarImageUrl);
--- tests/unit/helpers/gravatar-image-url-test.js
+++ tests/unit/helpers/gravatar-image-url-test.js
@@ -5,12 +5,12 @@ module('Unit | Helper | gravatar image url', function() {
   test('generates the URL to the image', function(assert) {
     let result = gravatarImageUrl(['joe@example.com'], {});
 
-    assert.equal(result, '//www.gravatar.com/avatar/f5b8fb60c6116331da07c65b96a8a1d1.jpg?d=mm&s=');
+    assert.equal(result, 'https://www.gravatar.com/avatar/f5b8fb60c6116331da07c65b96a8a1d1.jpg?d=mm&s=');
   });
 
   test('supports the size attribute', function(assert) {
     let result = gravatarImageUrl(['joe@example.com'], { size: 20 });
 
-    assert.equal(result, '//www.gravatar.com/avatar/f5b8fb60c6116331da07c65b96a8a1d1.jpg?d=mm&s=20');
+    assert.equal(result, 'https://www.gravatar.com/avatar/f5b8fb60c6116331da07c65b96a8a1d1.jpg?d=mm&s=20');
   });
 });
